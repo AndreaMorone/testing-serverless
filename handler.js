68680d8ed6b79bf5fdeeb0ea4018f804dc9297f6
Andrea Morone <andrea.morone88@gmail.com>
2020-07-19 19:29:13 +0200

    Update handler.js

--- handler.js
+++ handler.js
@@ -3,6 +3,6 @@ const test = require('./test');
 exports.handler = function(context, event) {
     var body = event.body.toString(); // event.body is a Buffer
     context.logger.info('reversing: ' + body);
-    context.logger.info('contenuto uguale a: ' + test);
+    context.logger.info('contenuto uguale a: ' + test());
     context.callback(body.split('').reverse().join(''));
 };
